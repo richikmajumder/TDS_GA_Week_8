4be4d1b32ed2183d8c303ae4f48a1740cd959e1a
richikmajumder <96072047+richikmajumder@users.noreply.github.com>
2022-11-30 16:05:44 +0530

    app.py

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ import streamlit as st
 st.write("""TDS week 8 Graded Assignment
 Build a streamlit web application for the following usecase and host the application using heroku.
 UseCase : Division of 2 given numbers . """)
-first_no = st.number_input("Enter first number")
-second_no = st.number_input("Enter second number")
-if second_no == 0:
+first_number = st.number_input("Enter first number")
+second_number = st.number_input("Enter second number")
+if second_number == 0:
   st.write("Division by zero not allowed")
   st.write("By Richik Majumder (21f1000460)")
 else:
-  division = first_no/second_no
-  st.write("The division of first_no by second_no is", str(division))
+  division = first_number/second_number
+  st.write("The division of first_number by second_number is", str(division))
   st.write("By Richik Majumder (21f1000460)")
